9148f7de621e4bf8a58af00e1447b1176958f9cd
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2018-09-19 00:07:56 +0530

    admin menu

--- application/views/site/user/administrator.php
+++ application/views/site/user/administrator.php
@@ -7,7 +7,46 @@
     </div>
 </div><!--/.heading-container-->
 
-<a class="dropdown-item" href="<?php echo base_url($this->router->directory.'user/manage'); ?>">Manage Employees</a>
+
+
+
+<div class="row">
+	<div class="col-md-3">
+		<a class="nav-link" href="<?php echo base_url($this->router->directory.'user/create_account'); ?>">
+			<div class="circle bg-primary text-white">
+				<div class="circle-body text-center p-4">
+					<i class="fa fa-user-plus fa-3x" aria-hidden="true"></i>
+					<p>Onboard New Employee</p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<div class="col-md-3">
+		<a class="nav-link" href="<?php echo base_url($this->router->directory.'user/manage'); ?>">
+			<div class="circle bg-primary text-white">
+				<div class="circle-body text-center p-4">
+					<i class="fa fa-user-plus fa-3x" aria-hidden="true"></i>
+					<p>Manage Employees</p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<div class="col-md-3">
+		<a class="nav-link" href="<?php echo base_url($this->router->directory.'user/timesheet'); ?>">
+			<div class="circle bg-primary text-white">
+				<div class="circle-body text-center p-4">
+					<i class="fa fa-user-plus fa-3x" aria-hidden="true"></i>
+					<p>Timesheet Tracker</p>
+				</div>
+			</div>
+		</a>
+	</div>
+</div><!--/.row-->
+
+
+
+
+<a class="dropdown-item" href="">Manage Employees</a>
 					<a class="dropdown-item" href="<?php echo base_url($this->router->directory.'user/create_account');?>">Add New Employee</a>
 					<a class="dropdown-item" href="<?php echo base_url($this->router->directory.'user/create_account');?>">Project Allocation</a>
 					<a class="dropdown-item" href="<?php echo base_url($this->router->directory.'timesheet'); ?>">View Timesheet</a>
