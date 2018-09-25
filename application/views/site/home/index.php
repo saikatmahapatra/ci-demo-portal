de99cf4b9b91485b89fb09710d7d927dc19dec9a
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2018-09-26 00:09:35 +0530

    carausal added

--- application/views/site/home/index.php
+++ application/views/site/home/index.php
@@ -1,4 +1,4 @@
-<div class="row heading-container">
+<div class="row heading-container mb-3">
     <div class="col-md-5">
         <h1 class="page-header"><?php echo isset($page_heading)? $page_heading:'Page Heading'; ?></h1>
     </div>
@@ -8,13 +8,53 @@
 </div><!--/.heading-container-->
 
 
+
+<div id="demo" class="carousel slide d-none" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo base_url('assets/src/img/la.jpg');?>" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo base_url('assets/src/img/chicago.jpg');?>" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo base_url('assets/src/img/ny.jpg');?>" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
 <?php
+$count = 1;
 foreach($data_rows as $key=>$row){
 	?>
-	<div class="row">
-		<div class="col-md-12 mb-4" data-id="<?php echo $row['id'];?>">
+	<?php if ($count%3 == 1){ echo '<div class="row mb-5">'; } ?>
+		<div class="col-md-4" data-id="<?php echo $row['id'];?>">
 			<div data-cms-type="<?php echo $row['pagecontent_type'];?>" class="card-news pl-2">
-				<div class="card-news-header h5">
+				<div class="card-news-header h6">
 					<a class="" href="<?php echo base_url($this->router->directory.$this->router->class.'/details/'.$this->common_lib->encode($row['id']));?>"><?php echo isset($row['pagecontent_title']) ? $row['pagecontent_title'] : '';?></a>
 				</div>
 				<div class="card-news-sig text-muted small">
@@ -27,14 +67,15 @@ foreach($data_rows as $key=>$row){
 				</div>
 				<div class="card-news-body">
 					<?php echo isset($row['pagecontent_text']) ? word_limiter($row['pagecontent_text'],50) : '';?>
-					<a class="" href="<?php echo base_url($this->router->directory.$this->router->class.'/details/'.$this->common_lib->encode($row['id']));?>">Read more</a>
-				
+					<a class="" href="<?php echo base_url($this->router->directory.$this->router->class.'/details/'.$this->common_lib->encode($row['id']));?>">Read more</a>				
 				</div>
 			</div>
-		</div>
+		<?php if ($count%3 == 0){ echo '</div>'; } ?>
 	</div>
 	<?php
+	$count++;
 }
+if ($count%3 != 1) echo "</div>"; 
 ?>
 
 <div class="row">
