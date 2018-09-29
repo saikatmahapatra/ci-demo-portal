d91aeec92cb1a17f14c52d0b6283c58de70e4423
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2018-09-29 17:55:19 +0530

    site down template and bug fixed

--- application/views/site/user/auth_error.php
+++ application/views/site/user/auth_error.php
@@ -1,7 +1,7 @@
 <div class="row justify-content-center">
     <div class="col-12 col-sm-8 col-md-6 text-center">
 		<i class="icon fa fa-warning fa-3x text-danger" aria-hidden="true"></i>
-		<h4 class="text-danger mt-3 mb-3"><?php echo isset($page_heading)? $page_heading : 'Page Heading'; ?></h5>
+		<h4 class="text-danger mt-3 mb-3"><?php echo isset($page_heading)? $page_heading : 'Page Heading'; ?></h4>
 		<h6>You are not authorized to access the link you clicked. For security reason you have been logged out. Please contact to system administrator for more details.</h6>
 		<a href="<?php echo base_url($this->router->directory.$this->router->class.'/login');?>" class="btn btn-primary mt-3"><i class="fa fa-fw fa-check-circle"></i> Please login to continue...</a>        
     </div>
