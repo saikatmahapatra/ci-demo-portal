3bb7ce4114f92f4f1f6889222d48a9098380c816
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2019-04-20 17:51:57 +0530

    version and news feed ui change

--- application/config/app_config.php
+++ application/config/app_config.php
@@ -53,6 +53,7 @@ $config['app_faq'] = array(
 
 $config['app_copy_right'] = 'Copyright '.date('Y').' &copy; United Exploration India Pvt. Ltd.';
 $config['app_admin_copy_right'] = 'Copyright &copy; '.date('Y').' <a href="http://unitedexploration.co.in/">United Exploration India Pvt. Ltd.</a>';
-$config['app_version'] = 'App Version '.CI_VERSION.'.16';
+//$config['app_version'] = 'App Version '.CI_VERSION.'.16';
+$config['app_version'] = 'App Version 3.2.16'; // CIVerMajor.RelVer.UIVer
 
 
