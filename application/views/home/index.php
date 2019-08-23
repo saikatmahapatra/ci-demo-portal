fc4008f58c238809017f1a707eec41f1382a49c4
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2019-08-23 22:04:59 +0530

    dashboard stat issue fixed

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -57,27 +57,42 @@
                 <div class="d-flex flex-column">
                     <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
                     
-                        <div class="pb-3 border-bottom">
-                            <a title="View Details" href="<?php echo base_url('user/manage'); ?>">
-                            <i class="fa fa-fw fa-id-badge fa-2x align-middle" aria-hidden="true" style="color: #a9289a;"></i> <span class="font-weight-bold"><?php echo isset($user_count) ? $user_count['data_rows'][0]['total'] : '0'; ?></span> employees
+                        <div class="stat media">
+                            <a title="View Details" class="d-flex" href="<?php echo base_url('user/manage'); ?>">
+                                <i class="fa fa-fw fa-id-badge fa-2x align-middle mr-2" aria-hidden="true" style="color: #a9289a;"></i> 
+                                
+                                <div class="media-body">
+                                    <span class="count"><?php echo isset($user_count) ? $user_count['data_rows'][0]['total'] : '0'; ?></span> employees
+                                </div>
                             </a>
                         </div>
 
-                        <div class="py-3 border-bottom">
-                            <a title="View Details" href="<?php echo base_url('project'); ?>">
-                            <i class="fa fa-fw fa-puzzle-piece fa-2x align-middle" aria-hidden="true" style="color: #007bff;"></i> <span class="font-weight-bold"><?php echo isset($projects_count) ? $projects_count['data_rows'][0]['total'] : '0'; ?></span> projects
+                        <div class="stat media">
+                            <a title="View Details" class="d-flex" href="<?php echo base_url('project'); ?>">
+                                <i class="fa fa-fw fa-puzzle-piece fa-2x align-middle mr-2" aria-hidden="true" style="color: #007bff;"></i> 
+
+                                <div class="media-body">
+                                    <span class="count"><?php echo isset($projects_count) ? $projects_count['data_rows'][0]['total'] : '0'; ?></span> projects
+                                </div>
                             </a>
                         </div>
 
-                        <div class="py-3 border-bottom">
-                            <a title="View Details" href="<?php echo base_url('timesheet/report'); ?>">
-                            <i class="fa fa-fw fa-clock-o fa-2x align-middle" aria-hidden="true" style="color: #495057;"></i> <span class="font-weight-bold"><?php echo isset($timesheet_user) ? $timesheet_user['data_rows'][0]['total'] : '0'; ?></span> users logged task today
+                        <div class="stat media">
+                            <a title="View Details" class="d-flex" href="<?php echo base_url('timesheet/report?form_action=search&from_date='.date('d-m-Y').'&to_date='.date('d-m-Y').''); ?>">
+                                <i class="fa fa-fw fa-clock-o fa-2x align-middle mr-2" aria-hidden="true" style="color: #495057;"></i> 
+
+                                <div class="media-body">
+                                    <span class="count"><?php echo isset($timesheet_user) ? $timesheet_user['data_rows'][0]['total'] : '0'; ?></span> employees logged task today
+                                </div>
                             </a>
                         </div>
 
-                        <div class="py-3 border-bottom">
-                            <a title="View Details" href="<?php echo base_url('leave/manage/all'); ?>">
-                                <i class="fa fa-fw fa-send-o fa-2x align-middle" aria-hidden="true" style="color: #fd7e14;"></i> <span class="font-weight-bold"><?php echo isset($user_approved_leave) ? $user_approved_leave['data_rows'][0]['total'] : '0'; ?></span> of <span class="font-weight-bold"><?php echo isset($user_applied_leave) ? $user_applied_leave['data_rows'][0]['total'] : '0'; ?></span> leave req. approved in <?php echo date('M');?>
+                        <div class="stat media">
+                            <a title="View Details" class="d-flex" href="<?php echo base_url('leave/manage/all'); ?>">
+                                <i class="fa fa-fw fa-send-o fa-2x align-middle mr-2" aria-hidden="true" style="color: #fd7e14;"></i> 
+                                <div class="media-body">
+                                    <span class="count"><?php echo isset($user_approved_leave) ? $user_approved_leave['data_rows'][0]['total'] : '0'; ?></span> of <span class="count"><?php echo isset($user_applied_leave) ? $user_applied_leave['data_rows'][0]['total'] : '0'; ?></span> leave requests for this month has been approved
+                                </div>
                             </a>
                         </div>
                     <?php } else{ ?>
@@ -86,15 +101,21 @@
                         </p> -->
                     <?php } ?>
 
-                    <div class="py-3 border-bottom">
-                        <a title="View Details" href="<?php echo base_url('timesheet/index'); ?>">
-                            <i class="fa fa-fw fa-list fa-2x align-middle" aria-hidden="true" style="color: #AC193D;"></i> <span class="font-weight-bold"><?php echo isset($user_timesheet_stat) ? $user_timesheet_stat['stat_data']['total_days'] : '0'; ?></span> days task logged by you in <?php echo date('M');?>
+                    <div class="stat media">
+                        <a title="View Details" class="d-flex" href="<?php echo base_url('timesheet/index'); ?>">
+                            <i class="fa fa-fw fa-list fa-2x align-middle mr-2" aria-hidden="true" style="color: #AC193D;"></i> 
+                            <div class="media-body">
+                                <span class="count"><?php echo isset($user_timesheet_stat) ? $user_timesheet_stat['stat_data']['total_days'] : '0'; ?></span> days task logged by you in this month
+                            </div>
                         </a>
                     </div>
 
-                    <div class="pt-3">
-                        <a title="View Details" href="<?php echo base_url('leave/manage/assigned_to_me'); ?>">
-                            <i class="fa fa-fw fa-check-square-o fa-2x align-middle" aria-hidden="true" style="color: #6f42c1;"></i> <span class="font-weight-bold"><?php echo isset($pending_leave_action) ? $pending_leave_action['data_rows'][0]['total'] : '0'; ?></span> leave req. waiting for your approval in <?php echo date('M');?>
+                    <div class="stat media">
+                        <a title="View Details" class="d-flex" href="<?php echo base_url('leave/manage/assigned_to_me'); ?>">
+                            <i class="fa fa-fw fa-check-square-o fa-2x align-middle mr-2" aria-hidden="true" style="color: #6f42c1;"></i> 
+                            <div class="media-body">
+                                <span class="count"><?php echo isset($pending_leave_action) ? $pending_leave_action['data_rows'][0]['total'] : '0'; ?></span> leave req. waiting for your approval in this month
+                            </div>
                         </a>
                     </div>
                 </div><!--/.flex-column-->
