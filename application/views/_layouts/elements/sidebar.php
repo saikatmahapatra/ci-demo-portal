b6ecdc8fea6996c1291354b6fafdf5e507954cbb
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-01-09 23:05:00 +0530

    sidebardark, light updated

--- application/views/_layouts/elements/sidebar.php
+++ application/views/_layouts/elements/sidebar.php
@@ -19,12 +19,12 @@
 
     <div class="sidebar-user">
         <a href="<?php echo base_url('user/profile'); ?>"><img class="sidebar-user-avatar" src="<?php echo base_url($img_src);?>" alt="Profile Image" /></a>
-        <div class="small">
+        <div class="">
             <p class="sidebar-user-name">
-                <?php echo isset($this->session->userdata['sess_user']['user_firstname']) ? $this->session->userdata['sess_user']['user_firstname'] : '' ;?>
+                <?php echo isset($this->session->userdata['sess_user']['user_firstname']) ? $this->session->userdata['sess_user']['user_firstname'].' ('.$this->session->userdata['sess_user']['user_emp_id'].')': '' ;?>
             </p>
-            <p class="sidebar-user-designation">
-                <?php echo isset($this->session->userdata['sess_user']['user_emp_id']) ? 'Emp ID :  '.$this->session->userdata['sess_user']['user_emp_id'] :'';?>
+            <p class="small sidebar-user-designation">
+                <?php echo isset($this->session->userdata['sess_user']['designation_name']) ? $this->session->userdata['sess_user']['designation_name'] :'';?>
             </p>
         </div>
     </div>
@@ -72,7 +72,7 @@
         
         <li class="treeview">
             <a class="menu-item" href="<?php echo base_url('#');?>" data-toggle="treeview">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
                 <span class="menu-label">Organization</span><i class="treeview-indicator fa fa-lg fa-angle-right" aria-hidden="true"></i>
             </a>
             <ul class="treeview-menu">
@@ -102,7 +102,7 @@
 
         <li class="treeview">
             <a class="menu-item" href="<?php echo base_url('#');?>" data-toggle="treeview">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                 <span class="menu-label">Timesheet</span><i class="treeview-indicator fa fa-lg fa-angle-right" aria-hidden="true"></i>
             </a>
             <ul class="treeview-menu">
@@ -117,7 +117,7 @@
 
         <li class="treeview">
             <a class="menu-item" href="<?php echo base_url('#');?>" data-toggle="treeview">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
                 <span class="menu-label">Leave</span><i class="treeview-indicator fa fa-lg fa-angle-right" aria-hidden="true"></i>
             </a>
             <ul class="treeview-menu">
@@ -138,7 +138,7 @@
         
         <li class="treeview">
             <a class="menu-item" href="<?php echo base_url('#');?>" data-toggle="treeview">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                 <span class="menu-label">My Account</span><i class="treeview-indicator fa fa-lg fa-angle-right" aria-hidden="true"></i>
             </a>
             <ul class="treeview-menu">
