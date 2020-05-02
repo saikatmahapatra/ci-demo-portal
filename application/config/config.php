150c0d7338ae775b7ed938a0ceec01feabbddd38
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-05-02 21:25:52 +0530

    session timeout 15 min inactivity

--- application/config/config.php
+++ application/config/config.php
@@ -360,7 +360,7 @@ $config['encryption_key'] = 'a1z3-d5rt-6k4n';
 */
 $config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_sessions';
-$config['sess_expiration'] = 0;
+$config['sess_expiration'] = 900;
 $config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
