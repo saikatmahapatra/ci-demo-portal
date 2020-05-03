53e2f78e6e092fd4885661eac9f9c45ae59b8030
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-05-03 17:27:34 +0530

    button color fix

--- application/views/errors/error_404.php
+++ application/views/errors/error_404.php
@@ -4,7 +4,7 @@
 		<div class="card ci-card">
 			<div class="card-body">
             <div class="alert alert-danger"><?php echo $this->common_lib->get_icon('warning'); ?> Sorry! The requested page could not found.</div>
-            <a href="<?php echo base_url();?>" class="btn btn-outline-primary">Go to home to contine</a>
+            <a href="<?php echo base_url();?>" class="btn btn-outline-secondary">Go to home</a>
 			</div><!--/.card-body-->
 		</div><!--/.card-->
 	</div>
