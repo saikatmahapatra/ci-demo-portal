eff0498ba7228b72378432fe803092870455ce5a
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-05-04 17:29:21 +0530

    self ac back link suppressed

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -90,7 +90,7 @@
 			<div class="card-header">Profile Card</div>
 			<div class="card-body">
 				
-				<?php if($this->session->userdata['sess_user']['user_role'] == 1) {?>
+				<?php if($this->session->userdata['sess_user']['user_role'] == 1 && !$is_self_account) {?>
 				<div class="d-flex mb-2">
 					<div class="align-self-end ml-auto">
 						<a href="<?php echo base_url($this->router->directory.'user/manage');?>" class="btn btn-outline-secondary"><?php echo $this->common_lib->get_icon('left_arrow'); ?> Back to Employee List</a>
