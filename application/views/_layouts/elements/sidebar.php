5a62102b55ecb8bd7b6e3d3d9b4fd7fd5bb875fd
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-05-05 23:03:28 +0530

    self service portal sside bar added user icon

--- application/views/_layouts/elements/sidebar.php
+++ application/views/_layouts/elements/sidebar.php
@@ -1,6 +1,10 @@
 <nav id="sidebar" class="<?php echo isset($this->session->userdata['sess_hide_sidebar_md']) ? 'active' : ''; ?>">
     <div class="sidebar-header">
-        <h3><a href="<?php echo base_url($this->router->directory); ?>">Self Service Portal</a></h3>
+        <div class="sidebar-user-dp mx-auto d-block">
+            AB
+        </div>
+        <!-- <h3><a href="<?php echo base_url($this->router->directory); ?>">Self Service Portal</a></h3> -->
+        <div class="text-center my-2">Antony A Broadmanistia P</div>
         <div id="locksidebar" class="d-none d-md-block float-right"><?php echo isset($this->session->userdata['sess_hide_sidebar_md']) ? $this->common_lib->get_icon('right_arrow') : $this->common_lib->get_icon('left_arrow'); ?></div>
     </div>
     
