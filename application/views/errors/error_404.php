5bfc5fc6951c4a98772038b0f1098d981d944ffc
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-08-05 09:38:58 +0530

    images added

--- application/views/errors/error_404.php
+++ application/views/errors/error_404.php
@@ -3,7 +3,7 @@
 	<div class="row justify-content-center">
 		<div class="col-lg-6">
 			<div class="text-center mt-4">
-				<img class="mb-4 img-error" src="<?php echo base_url('assets/dist/img/error-404-monochrome.svg');?>" />
+				<img class="mb-4 img-error" src="<?php echo base_url('assets/dist/img/404-error-with-a-cute-animal-pana.svg');?>" />
 				<p class="lead">This requested URL was not found on this server.</p>
 				<a href="<?php echo base_url();?>">
 					Go to Dashboard <i class="fas fa-arrow-right ml-1"></i>
