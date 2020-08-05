0c89a836557a6753ae40f440833aac8db344c6b0
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-08-05 11:18:09 +0530

    config change

--- application/config/config.php
+++ application/config/config.php
@@ -513,4 +513,4 @@ $config['proxy_ips'] = '';
 | $config['maintenance_mode'] = TRUE; // site is offline
 | $config['maintenance_mode'] = FALSE; // site is online
 */
-$config['maintenance_mode'] = true;
+$config['maintenance_mode'] = FALSE;
