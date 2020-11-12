2b109ebbbbeff143a322cf809257db6cecb99168
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-11-13 00:03:21 +0530

    navbar btn updated

--- application/views/_layouts/elements/navbar.php
+++ application/views/_layouts/elements/navbar.php
@@ -1,10 +1,10 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary shadow">
+<nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
     <a class="navbar-brand" href="<?php echo base_url($this->router->directory); ?>">
     <!-- <img class="mx-auto" src="<?php echo base_url('assets/dist/img/logo-nav.png');?>" alt="Logo" width="48px">  -->
     My App</a>
     <button class="btn btn-link order-1 order-lg-0" id="sidebarToggle">
-        <i class="navmenu-icon fa fa-bars"></i>
-        <!-- <span class="navmenu-icon navbar-toggler-icon"></span> -->
+        <!-- <i class="navmenu-icon fa fa-bars"></i> -->
+        <span class="navmenu-icon navbar-toggler-icon"></span>
     </button>
     <!-- Navbar Search-->
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
