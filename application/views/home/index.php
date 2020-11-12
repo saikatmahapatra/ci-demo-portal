4a5a33007476c79bbc04519619ef25832a144646
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-11-12 21:00:40 +0530

    new dashboard implemented

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,8 +1,8 @@
 <h1 class="page-title"><?php echo isset($page_title) ? $page_title : 'Page Heading'; ?></h1>
 <?php //echo isset($breadcrumbs) ? $breadcrumbs : ''; ?>
 
-<div class="row mb-3">
-    <div class="col-md-8">
+<div class="row">
+    <div class="col-md-8 mb-3">
         <div class="card news-card ">
             <div class="card-header"><?php echo $this->common_lib->get_icon('notice_board'); ?> Organizational Announcements</div>
             <div class="card-body">
@@ -10,7 +10,7 @@
             <?php if( isset($data_rows) && sizeof($data_rows) > 0 ){ ?>
             <div class="list-group list-group-flush">
                 <?php foreach($data_rows as $key=>$row) { ?>
-                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <a href="<?php echo base_url('home/details/'.$row['id']);?>" class="list-group-item list-group-item-action flex-column align-items-start">
                         <div class="d-flex w-100 justify-content-between">
                         <div class="mb-1"><?php echo isset($row['content_title']) ? $row['content_title'] : '';?></div>
                         <small class="d-none d-md-block  text-muted"><?php echo $this->common_lib->relative_time($row['content_created_on'],true,null); ?></small>
@@ -32,98 +32,87 @@
         <div class="card ">
             <div class="card-header"><?php echo $this->common_lib->get_icon('stat'); ?> Statistics</div>
             <div class="card-body">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="list-group list-group-flush">
-                            <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
-                            <a target="_blank" href="<?php echo $dashboard_stat['user']['url'];?>" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                                Employees
-                                <span class="badge badge-primary badge-pill"><?php echo $dashboard_stat['user']['count'];?></span>
-                            </a>
-                            <?php } ?>
-                            <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
-                            <a target="_blank" href="<?php echo $dashboard_stat['timesheet_user']['url'];?>" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                                Employees logged task *
-                                <span class="badge badge-dark badge-pill"><?php echo $dashboard_stat['timesheet_user']['count'];?></span>
-                            </a>
-                            <?php } ?>
-                            <a target="_blank" href="<?php echo $dashboard_stat['timesheet_days']['url'];?>" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                                Days you logged task *
-                                <span class="badge badge-info badge-pill"><?php echo $dashboard_stat['timesheet_days']['count'];?></span>
-                            </a>
-                            <a target="_blank" href="<?php echo $dashboard_stat['leave_to_approve']['url'];?>" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                                Leave awaiting for your aproval
-                                <span class="badge badge-danger badge-pill"><?php echo $dashboard_stat['leave_to_approve']['count'];?></span>
-                            </a>
-                        </div>
-                        <div></div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div class="list-group list-group-flush">
-                            <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
-                            <a target="_blank" href="<?php echo $dashboard_stat['project']['url'];?>" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                                Projects
-                                <span class="badge badge-secondary badge-pill"><?php echo $dashboard_stat['project']['count'];?></span>
-                            </a>
-                            <?php } ?>
-                            <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
-                            <a target="_blank" href="<?php echo $dashboard_stat['user_applied_leave']['url'];?>" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                                Leave approved *
-                                <span class="badge badge-info badge-pill"><?php echo $dashboard_stat['user_applied_leave']['count'];?></span>
-                            </a>
-                            <?php } ?>
-                            <a target="_blank" href="<?php echo $dashboard_stat['timesheet_hrs']['url'];?>" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                                Your logged hours *
-                                <span class="badge badge-warning badge-pill"><?php echo $dashboard_stat['timesheet_hrs']['count'];?></span>
-                            </a>
-                            <a target="_blank" href="<?php echo $dashboard_stat['timesheet_avg_hrs']['url'];?>" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                                Your average logged hours *
-                                <span class="badge badge-success badge-pill"><?php echo $dashboard_stat['timesheet_avg_hrs']['count'];?></span>
-                            </a>
-                        </div>
-                        <div></div>
-                    </div>
-                    <div class="col-12 my-2 small text-sm-center text-lg-right text-muted">* Statistics of current month.</div>
-
-                    <?php /* foreach($dashboard_stat as $key=>$data) { ?>
-                        <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
-                        <div class="col-sm-6 col-md-3 col-lg-3">
-                            <div class="mb-3 text-center<?php echo $data['bg_css']; echo $data['text_css'];?> ">
-                                <div class="">
-                                    <div class="digit <?php echo $data['digit_css'];?>"><?php echo $data['count'];?></div>
-                                    <div class=""><a href="<?php echo isset($data['url']) ? $data['url'] : '#';?>"><?php echo $data['heading'];?></a></div>
-                                    <div class=""><?php echo $data['info_text'];?></div>
+                <div class="d-flex flex-column dashboard-stat">
+                    <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
+                        <div class="media">
+                            <a class="d-flex" href="<?php echo $dashboard_stat['user']['url'];?>">
+                                <i class="fa fa-fw fa-user fa-2x align-middle" aria-hidden="true" style="color: #0062cc;"></i>
+                                <div class="media-body">
+                                    <span class="count"><?php echo $dashboard_stat['user']['count'];?></span> employees
                                 </div>
-                            </div>
+                            </a>
                         </div>
-                        <?php } else {?>
-                            <?php if($data['target_role'] !== '1'){
-                                ?>
-                                <div class="col-sm-6 col-md-3 col-lg-3">
-                                    <div class="mb-3 text-center <?php echo $data['bg_css']; echo $data['text_css'];?> ">
-                                        <div class="">
-                                            <div class="digit <?php echo $data['digit_css'];?>"><?php echo $data['count'];?></div>
-                                            <div class=""><a href="<?php echo isset($data['url']) ? $data['url'] : '#';?>"><?php echo $data['heading'];?></a></div>
-                                            <div class=""><?php echo $data['info_text'];?></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <?php 
-                            }?>
-                        <?php } ?>
                     <?php } ?>
-
-                    <?php if($data['target_role'] !== '1'){ ?>
-                        <div class="col small text-sm-center text-lg-right text-muted">* Calculated based on your timesheet data of current month.</div>
-                    <?php } */ ?>
-            </div>
+                    <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
+                        <div class="media">
+                            <a class="d-flex" href="<?php echo $dashboard_stat['project']['url'];?>">
+                                <i class="fa fa-fw fa-puzzle-piece fa-2x align-middle" aria-hidden="true" style="color: #fd7e14;"></i>
+                                <div class="media-body">
+                                    <span class="count"><?php echo $dashboard_stat['project']['count'];?></span> projects
+                                </div>
+                            </a>
+                        </div>
+                    <?php } ?>
+                    <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
+                        <div class="media">
+                            <a class="d-flex" href="<?php echo $dashboard_stat['timesheet_user']['url'];?>">
+                                <i class="fa fa-fw fa-check-square-o fa-2x align-middle" aria-hidden="true" style="color: #6f42c1;"></i>
+                                <div class="media-body">
+                                    <span class="count"><?php echo $dashboard_stat['timesheet_user']['count'];?></span> employees logged task*
+                                </div>
+                            </a>
+                        </div>
+                    <?php } ?>
+                    <?php if ($this->session->userdata['sess_user']['user_role'] == 1) { ?>
+                        <div class="media">
+                            <a class="d-flex" href="<?php echo $dashboard_stat['user_applied_leave']['url'];?>">
+                                <i class="fa fa-fw fa-paper-plane-o fa-2x align-middle" aria-hidden="true" style="color: #28a745;"></i>
+                                <div class="media-body">
+                                    <span class="count"><?php echo $dashboard_stat['user_applied_leave']['count'];?></span> leaves approved
+                                </div>
+                            </a>
+                        </div>
+                    <?php } ?>
+                    <div class="media">
+                        <a class="d-flex" href="<?php echo $dashboard_stat['leave_to_approve']['url'];?>">
+                            <i class="fa fa-fw fa-list-ul fa-2x align-middle" aria-hidden="true" style="color: #6c757d;"></i>
+                            <div class="media-body">
+                                <span class="count"><?php echo $dashboard_stat['leave_to_approve']['count'];?></span> leave awaiting for your aproval
+                            </div>
+                        </a>
+                    </div>
+                    <div class="media">
+                        <a class="d-flex" href="<?php echo $dashboard_stat['timesheet_days']['url'];?>">
+                            <i class="fa fa-fw fa-calendar fa-2x align-middle" aria-hidden="true" style="color: #dc3545;"></i>
+                            <div class="media-body">
+                                <span class="count"><?php echo $dashboard_stat['timesheet_days']['count'];?></span> days you logged task*
+                            </div>
+                        </a>
+                    </div>
+                    <div class="media">
+                        <a class="d-flex" href="<?php echo $dashboard_stat['timesheet_hrs']['url'];?>">
+                            <i class="fa fa-fw fa-clock-o fa-2x align-middle" aria-hidden="true" style="color: #007bff;"></i>
+                            <div class="media-body">
+                                <span class="count"><?php echo $dashboard_stat['timesheet_hrs']['count'];?></span> hrs you logged tasks
+                            </div>
+                        </a>
+                    </div>
+                    <div class="media">
+                        <a class="d-flex" href="<?php echo $dashboard_stat['timesheet_avg_hrs']['url'];?>">
+                            <i class="fa fa-fw fa-code fa-2x align-middle" aria-hidden="true" style="color: #0062cc;"></i>
+                            <div class="media-body">
+                                <span class="count"><?php echo $dashboard_stat['timesheet_avg_hrs']['count'];?></span> hrs avg. you logged
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="my-2 small text-muted">* Statistics of current month.</div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="row mb-3">
+<div class="row mt-3">
     <div class="col">
         <div class="card ">
             <div class="card-header"><?php echo $this->common_lib->get_icon('calendar'); ?> Calendar</div>
