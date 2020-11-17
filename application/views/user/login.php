009fddc6b7c331a7dffa0a291aa6c4c7d8218057
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-11-17 18:47:27 +0530

    sign on form

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -3,7 +3,7 @@
   <div class="mb-2"><?php echo isset($alert_message) ? $alert_message : ''; ?></div>
   <a href="<?php echo base_url();?>"><img class="mb-4" src="<?php echo base_url('assets/dist/img/logo-nav.png');?>" alt="Logo" width="100"></a>
   <h1 class="h3 mb-3 font-weight-normal"><?php echo isset($page_title) ? $page_title : 'Page Heading'; ?></h1>
-  <div class="form-group mb-0">
+  <div class="form-group">
     <label class="sr-only" for="user_email">Email</label>
     <?php echo form_input(array('name' => 'user_email', 'value' => set_value('user_email'),'id' => 'user_email','class' => 'form-control py-4','placeholder' => 'Enter email address','maxlength' => '100','autofocus' => true,));?>
     <?php echo form_error('user_email'); ?>
