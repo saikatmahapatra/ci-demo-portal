2e9e876b69222583a9a06b1a3de3799cbe0dace0
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-12-07 00:38:35 +0530

    sidebar, footer style updated

--- application/views/_layouts/elements/footer.php
+++ application/views/_layouts/elements/footer.php
@@ -1,7 +1,7 @@
 <footer class="py-4 mt-auto footer-dark">
     <div class="container-fluid small">
         <div class="text-center">
-            <div><?php echo $this->config->item('app_admin_copy_right');?> &nbsp;</div>
+            <div><?php echo $this->config->item('app_admin_copy_right');?></div>
             <div><?php echo $this->config->item('app_version');?></div>
         </div>
     </div>
