0941e58b050824e9eba1ccd7bdbbfd3875e0aa09
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-12-24 11:38:39 +0530

    Document controller merged to User

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2264,6 +2264,140 @@ class User extends CI_Controller {
 				return false;
 			}
 		}
-	}
+    }
+    
+    function my_documents() {
+        $is_logged_in = $this->common_lib->is_logged_in();
+        if ($is_logged_in == FALSE) {
+            redirect($this->router->directory.$this->router->class.'/login');
+        }
+        //Has logged in user permission to access this page or method?
+        /*$is_authorized = $this->common_lib->is_auth(array(
+            'default-super-admin-access',
+            'default-admin-access'
+        ));*/
+        $this->data['id'] = $this->uri->segment(3) ? $this->uri->segment(3) : $this->sess_user_id;
+        //Uploads
+        $upload_related_to = 'user';
+        $this->data['upload_related_to'] = $upload_related_to;
+        $this->data['all_uploads'] = $this->user_model->get_uploads($upload_related_to, $this->data['id'], NULL, NULL);
+        if ($this->input->post('form_action') == 'file_upload') {
+            $this->upload_my_documents_file();
+        }
+		$this->data['page_title'] = 'My Documents';
+        $this->data['maincontent'] = $this->load->view($this->router->class.'/my_documents', $this->data, true);
+        $this->load->view('_layouts/layout_default', $this->data);
+    }
+
+    function upload_my_documents_file() {
+        $is_logged_in = $this->common_lib->is_logged_in();
+        if ($is_logged_in == FALSE) {
+            redirect($this->router->directory.$this->router->class.'/login');
+        }
+        if ($this->validate_uplaod_my_document_form_data() == true) {
+            $upload_related_to = 'user'; // related to user, product, album, contents etc
+            $upload_related_to_id = $this->data['id']; // related to id user id, product id, album id etc
+            $upload_file_type_name = $this->input->post('upload_file_type_name'); // file type name
+            //Create directory for object specific
+            $upload_path = 'assets/uploads/' . $upload_related_to . '/docs/' . $upload_related_to_id;
+            if (!is_dir($upload_path)) {
+                mkdir($upload_path, 0777, TRUE);
+                copy('assets/index.html', $upload_path.'/index.html');
+            }
+            $allowed_ext = 'png|jpg|jpeg|doc|docx|pdf';
+            $upload_param = array(
+                'upload_path' => $upload_path, // original upload folder
+                'allowed_types' => $allowed_ext, // allowed file types,
+                'max_size' => '300', // max 300KB,
+                'file_new_name' => $upload_related_to_id . '_' . $upload_file_type_name . '_' . time(),
+            );
+            $upload_result = $this->common_lib->upload_file('userfile', $upload_param);
+            if (isset($upload_result['file_name']) && empty($upload_result['upload_error'])) {
+                $uploaded_file_name = $upload_result['file_name'];
+                $postdata = array(
+                    'upload_related_to' => $upload_related_to,
+                    'upload_related_to_id' => $upload_related_to_id,
+                    'upload_file_type_name' => $upload_file_type_name,
+                    'upload_file_name' => $uploaded_file_name,
+                    'upload_mime_type' => $upload_result['file_type'],
+                    'upload_by_user_id' => $this->sess_user_id,
+                    'upload_datetime' => date('Y-m-d H:i:s')
+                );
+
+                // Allow mutiple file upload for a file type.
+                $multiple_allowed_upload_file_type = array('work_exp_letter');
+                if (!in_array($upload_file_type_name, $multiple_allowed_upload_file_type)) {
+                    $uploads = $this->user_model->get_uploads($upload_related_to, $upload_related_to_id, NULL, $upload_file_type_name);
+                }
+                if (isset($uploads[0]) && ($uploads[0]['id'] != '')) {
+                    //Unlink previously uploaded file
+                    $file_path = $upload_param['upload_path'] . '/' . $uploads[0]['upload_file_name'];
+                    if (file_exists(FCPATH . $file_path)) {
+                        $this->common_lib->unlink_file(array(FCPATH . $file_path));
+                    }
+                    // Now update table
+                    $update_upload = $this->user_model->update($postdata, array('id' => $uploads[0]['id']), 'uploads');
+                    $this->common_lib->set_flash_message('File has been uploaded successfully.','alert-success');
+                    redirect(current_url());
+                } else {
+                    $upload_insert_id = $this->user_model->insert($postdata, 'uploads');
+                    $this->common_lib->set_flash_message('File has been uploaded successfully.','alert-success');
+                    redirect(current_url());
+                }
+            } else if (sizeof($upload_result['upload_error']) > 0) {
+                $error_message = $upload_result['upload_error'];
+                $this->common_lib->set_flash_message($error_message,'alert-danger');
+                redirect(current_url());
+            }
+        }
+    }
+
+    function validate_uplaod_my_document_form_data() {
+        $this->form_validation->set_rules('upload_file_type_name', ' ', 'required');
+        if (empty($_FILES['userfile']['name'])){
+			$this->form_validation->set_rules('userfile', ' ', 'required');
+		}
+        $this->form_validation->set_error_delimiters('<div class="validation-error">', '</div>');
+        if ($this->form_validation->run() == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function delete_file() {
+        //Check if any user logged in else redirect to login
+        $is_logged_in = $this->common_lib->is_logged_in();
+        if ($is_logged_in == FALSE) {
+			$this->session->set_userdata('sess_post_login_redirect_url', current_url());
+            redirect($this->router->directory.'user/login');
+        }
+
+        $id = $this->input->get_post('id');
+        $file_path = $this->input->get_post('file_path');
+        if ($id) {
+            $where_array = array('id' => $id);
+            $res = $this->user_model->delete($where_array, 'uploads');
+            if ($res) {
+                $this->common_lib->unlink_file(array(FCPATH . $file_path));
+            }
+            echo json_encode("success");
+        } else {
+            echo json_encode("error");
+        }
+    }
+    
+    function delete_uploads($upload_related_to, $upload_related_to_id) {
+        $is_logged_in = $this->common_lib->is_logged_in();
+        if ($is_logged_in == FALSE) {
+            redirect($this->router->directory.$this->router->class.'/login');
+        }
+        $where_array = array('upload_related_to' => $upload_related_to, 'upload_related_to_id' => $upload_related_to_id);
+        $res = $this->user_model->delete($where_array, 'uploads');
+        if ($res) {
+            $upload_path = 'assets/uploads/'.$upload_related_to.'/' . $upload_related_to_id;
+            $this->common_lib->recursive_remove_directory(FCPATH . $upload_path);
+        }
+    }
 }
 ?>
--- assets/src/js/user.js
+++ assets/src/js/user.js
@@ -16,6 +16,10 @@ function domReady() {
         renderEmployeesDataTable();
     }
 
+    if (ROUTER_METHOD === 'my_documents') {
+        $('.btn-delete-file').on('click', deleteUploadedFiles);
+    }
+
     //Date of Birth Date Picker
     $('#user_dob').datepicker({
         format: "dd-mm-yyyy",
@@ -472,4 +476,50 @@ function get_user_suggestion() {
     // promise.always(function() {
 
     // });
+}
+
+function deleteUploadedFiles(e) {
+    e.preventDefault();
+    var that = $(this);
+    var alert_msg = '';
+    var data_row = that.parents('.file-container');
+    var clickedBtn = that;
+    alert_msg += 'Are you sure you want to delete this file?\n';
+    var conf = confirm(alert_msg);
+
+    if (conf == true) {
+        var upload_id = that.attr('data-upload_id');
+        var file_path = that.attr('data-path');
+
+        var xhr = new Ajax();
+        xhr.type = 'POST';
+        xhr.url = SITE_URL + ROUTER_DIRECTORY + ROUTER_CLASS + '/delete_file';
+        xhr.beforeSend = function() {
+            showAjaxLoader();
+        };
+        xhr.data = { id: upload_id, file_path: file_path };
+        var promise = xhr.init();
+        promise.done(function(response) {
+            if (response == 'success') {
+                data_row.remove();
+                hideAjaxLoader();
+            }
+        });
+        promise.fail(function() {
+            alert("Failed");
+        });
+        promise.always(function() {
+            clickedBtn.html('Delete');
+        });
+
+    } else {
+        return false;
+    }
+}
+
+function displayFile(e) {
+    e.preventDefault();
+    var path = $(this).attr('href');
+    $('#exampleModal').modal('show');
+    $('#file-container-object').attr('data', path);
 }
