eeb8e4fd0a15081d6d7d249a858a221f72e95ce5
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-04-28 00:24:49 +0530

    UI modified

--- application/views/user/forgot_password.php
+++ application/views/user/forgot_password.php
@@ -1,11 +1,10 @@
 <div class="row justify-content-center align-items-center" style="height:100vh">
    <div class="col-sm-6 col-lg-4 login-card-wrapper">
-      <div class="text-center mb-3"><a href="<?php echo base_url();?>"><img class="" src="<?php echo base_url('assets/dist/img/logo-nav.png');?>" alt="Logo" width="100"></a></div>
       <div class="card">
-         <!-- <div class="card-header"></div> -->
+         <div class="card-header py-3 text-center h5"><?php echo isset($page_title) ? $page_title : 'Page Heading'; ?></div>
          <div class="card-body">
             <div class="mb-2"><?php echo isset($alert_message) ? $alert_message : ''; ?></div>
-            <h4 class="text-center"><?php echo isset($page_title) ? $page_title : 'Page Heading'; ?></h4>
+            <div class="text-center mb-4"><a href="<?php echo base_url();?>"><img class="" src="<?php echo base_url('assets/dist/img/logo-nav.png');?>" alt="Logo" width="80"></a></div>
             <p class="text-center text-muted">Enter your email address and we will send you an OTP to reset your password.</p>
             <?php echo form_open(current_url(), array('method' => 'post', 'class'=>'form-signin mt-3')); ?>
             <?php echo form_hidden('form_action', 'forgot_password'); ?>
@@ -20,8 +19,7 @@
          </div>
       </div>
       <div class="text-center text-muted mt-4 small">
-        <div><?php echo $this->config->item('app_admin_copy_right');?></div>
-        <div><?php echo $this->config->item('app_version');?></div>
+        <div><?php echo $this->config->item('app_admin_copy_right');?> <?php echo $this->config->item('app_version');?></div>
       </div>
    </div>
 </div>
--- application/views/user/reset_password.php
+++ application/views/user/reset_password.php
@@ -1,11 +1,10 @@
 <div class="row justify-content-center align-items-center" style="height:100vh">
    <div class="col-sm-6 col-lg-4 login-card-wrapper">
-      <div class="text-center mb-3"><a href="<?php echo base_url();?>"><img class="" src="<?php echo base_url('assets/dist/img/logo-nav.png');?>" alt="Logo" width="100"></a></div>
       <div class="card">
-         <!-- <div class="card-header"></div> -->
+         <div class="card-header py-3 text-center h5"><?php echo isset($page_title) ? $page_title : 'Page Heading'; ?></div>
          <div class="card-body">
             <div class="mb-2"><?php echo isset($alert_message) ? $alert_message : ''; ?></div>
-            <h4 class="text-center"><?php echo isset($page_title) ? $page_title : 'Page Heading'; ?></h4>
+            <div class="text-center mb-4"><a href="<?php echo base_url();?>"><img class="" src="<?php echo base_url('assets/dist/img/logo-nav.png');?>" alt="Logo" width="80"></a></div>
             <?php echo form_open(current_url(), array('method' => 'post', 'class'=>'form-signin mt-3')); ?>
             <?php echo form_hidden('form_action', 'reset_password'); ?>
             <?php echo form_hidden('user_email', isset($_POST['user_email']) ? set_value('user_email') : $this->session->userdata('sess_forgot_password_username')); ?>
@@ -34,8 +33,7 @@
          </div>
 	  </div>
 	  <div class="text-center text-muted mt-4 small">
-        <div><?php echo $this->config->item('app_admin_copy_right');?></div>
-        <div><?php echo $this->config->item('app_version');?></div>
+        <div><?php echo $this->config->item('app_admin_copy_right');?> <?php echo $this->config->item('app_version');?></div>
       </div>
    </div>
 </div>
