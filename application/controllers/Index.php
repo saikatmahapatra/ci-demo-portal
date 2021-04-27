510a64644f3a75227eb2386fdafd4d8975a68bd2
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-04-28 00:05:17 +0530

    app renamed to index

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class App extends CI_Controller {
+class Index extends CI_Controller {
 
     function __construct() {
         parent::__construct();
@@ -12,7 +12,7 @@ class App extends CI_Controller {
 
 	function index() {
         $this->data['title'] = 'Please wait. You will be redirected in <span id="countdown">10</span> seconds...';
-        $this->load->view('app', $this->data);
+        $this->load->view('index', $this->data);
     }
 }
 ?>
