8cc162272b52d39ff384c832159728c310f8c51b
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-04-28 09:44:31 +0530

    footeer fixed

--- application/views/_layouts/elements/footer.php
+++ application/views/_layouts/elements/footer.php
@@ -1,4 +1,4 @@
-<footer class="py-4 mt-auto footer-dark">
+<footer class="py-4 mt-auto footer-dark small">
     <div class="container">
         <div class="text-center">
             <div><?php echo $this->config->item('app_admin_copy_right');?> <?php echo $this->config->item('app_version');?></div>
