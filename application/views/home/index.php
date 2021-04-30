bcb16eb5e308c53ec7f4555df5ca58140589032a
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-04-30 21:19:40 +0530

    news display modified

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -50,27 +50,21 @@
 <div class="row">
     <div class="col-md-8 mb-3">
         <div class="card news-card ">
-            <div class="card-header"><?php echo $this->common_lib->get_icon('notice_board'); ?> News</div>
+            <div class="card-header"><?php echo $this->common_lib->get_icon('notice_board'); ?> Latest News</div>
             <div class="card-body">
             
             <?php if( isset($data_rows) && sizeof($data_rows) > 0 ){ ?>
-            <div class="list-group list-group-flush">
                 <?php foreach($data_rows as $key=>$row) { ?>
-                    <a href="<?php echo base_url('home/details/'.$row['id']);?>" class="list-group-item list-group-item-action flex-column align-items-start">
-                        <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1"><?php echo isset($row['content_title']) ? $row['content_title'] : '';?></div>
-                        <small class="d-none d-md-block  text-muted"><?php echo $this->common_lib->relative_time($row['content_created_on'],true,null); ?></small>
-                        </div>
-                        <!-- <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p> -->
-                        <small class="d-xs-block d-md-none text-muted"><?php echo $this->common_lib->relative_time($row['content_created_on'],true,null); ?></small>
-                        <small class="text-muted">by <?php echo isset($row['user_firstname']) ? $row['user_firstname'].' '.$row['user_lastname'] : '';?></small>
-                    </a>
+                    <div class="news-item mb-3">
+                        <div><a href="<?php echo base_url('home/details/'.$row['id']);?>" class=""><?php echo isset($row['content_title']) ? $row['content_title'] : '';?></a></div>
+                        <div class="small text-muted"><?php echo $this->common_lib->relative_time($row['content_created_on'],true,null); ?></div>
+                        <div class="small text-muted">by <?php echo isset($row['user_firstname']) ? $row['user_firstname'].' '.$row['user_lastname'] : '';?></div>
+                    </div>
                 <?php }  ?>
-            </div>
             <?php } ?>
             </div>
-            <div class="text-center pt-0">
-                <a class="btn btn-link action-link" href="<?php echo base_url('home/all_news');?>">View more <?php echo $this->common_lib->get_icon('right_fwd'); ?></a>
+            <div class="text-center mb-3">
+                <a class="btn btn-primary action-link" href="<?php echo base_url('home/all_news');?>">View more...</a>
             </div>
         </div><!--/.card-->
     </div>
